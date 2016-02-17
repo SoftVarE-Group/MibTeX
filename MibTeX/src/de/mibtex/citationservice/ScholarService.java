7247a414ad2c0771a387fb7a0627db267e3a9dfd
Thomas Thüm <thomas.thuem@ovgu.de>
2016-02-17 22:29:26 +0100

    Fixed #11 and #13: citation order is not touched by citation service and read again before every update

--- MibTeX/src/de/mibtex/citationservice/ScholarService.java
+++ MibTeX/src/de/mibtex/citationservice/ScholarService.java
@@ -14,7 +14,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -22,19 +21,22 @@ import java.util.Random;
  * A class that reads all BibTeX entries from a .csv file, gets the citations and put the entries back
  * in the .csv file
  * 
- * @author Christopher Sontag
+ * @author Christopher Sontag, Thomas Thuem
  */
 public class ScholarService extends Thread {
     
-    private List<CitationEntry> entries;
-    private Random rand;
+    private static final String CITATIONS_FILE = "citations.csv";
+    
+	private Random rand = new Random();
     
     @Override
     public void run() {
-        for (CitationEntry entry : entries) {
+        while (true) {
+            List<CitationEntry> entries = readFromFile(CITATIONS_FILE);
+            CitationEntry entry = nextEntry(entries);
             entry.updateCitations();
-            System.out.println(entry.getKey() + " is cited " + entry.getCitations());
-            writeToFile("citations.csv", entries);
+            System.out.println(entry.getCitations() + " citations of " + entry.getKey() + " with title \"" + entry.getTitle() + "\"");
+            writeToFile(CITATIONS_FILE, entries);
             try {
                 sleep(rand.nextInt(120000) + 120000);
             } catch (InterruptedException e) {
@@ -43,23 +45,24 @@ public class ScholarService extends Thread {
         }
     }
     
-    public ScholarService() throws Exception {
-        rand = new Random(120000);
-        entries = readFromFile("citations.csv", ";");
-        Collections.sort(entries);
+    protected CitationEntry nextEntry(List<CitationEntry> entries) {
+    	CitationEntry next = entries.get(0);
+    	for (CitationEntry entry : entries)
+            if (entry.getLastUpdate() < next.getLastUpdate())
+            	next = entry;
+		return next;
     }
     
-    protected List<CitationEntry> readFromFile(String filename, String delimeter) {
+    protected List<CitationEntry> readFromFile(String filename) {
         List<CitationEntry> entries = new ArrayList<CitationEntry>();
         try (BufferedReader br = new BufferedReader(new FileReader(new File(CitationService.CITATION_DIR
                 +"\\"+ filename)))) {
             for (String line; (line = br.readLine()) != null;) {
-                String[] str = line.split(delimeter);
+                String[] str = line.split(";");
                 String key = replaceCSVSpeficics(str[0]);
                 String title = replaceCSVSpeficics(str[1]);
                 int citations = Integer.parseInt(str[2]);
                 long lastUpdate = Long.parseLong(str[3]);
-                
                 entries.add(new CitationEntry(key, title, citations, lastUpdate));
             }
         } catch (IOException e) {
@@ -76,21 +79,18 @@ public class ScholarService extends Thread {
     protected void writeToFile(String filename, List<CitationEntry> entries) {
         try {
             File file = new File(CitationService.CITATION_DIR +"\\"+ filename);
-            List<CitationEntry> oldContent = readFromFile(filename, ";");
-            if (!entries.equals(oldContent)) {
-                System.out.println("Updating " + filename);
-                BufferedWriter out = new BufferedWriter(new FileWriter(file));
-                StringBuilder CSV = new StringBuilder();
-                for (CitationEntry entry : entries) {
-                    CSV.append("\"" + entry.getKey() + "\";").append("\"" + entry.getTitle() + "\";")
-                            .append(entry.getCitations() + ";").append(entry.getLastUpdate() + ";")
-                            .append(System.getProperty("line.separator"));
-                }
-                out.write(CSV.toString());
-                out.close();
+            System.out.println("Updating " + filename);
+            BufferedWriter out = new BufferedWriter(new FileWriter(file));
+            for (CitationEntry entry : entries) {
+                out.append("\"" + entry.getKey() + "\";");
+                out.append("\"" + entry.getTitle() + "\";");
+                out.append(entry.getCitations() + ";");
+                out.append(entry.getLastUpdate() + ";");
+                out.append(System.getProperty("line.separator"));
             }
+            out.close();
         } catch (FileNotFoundException e) {
-            System.out.println("Not Found " + filename);
+            System.out.println("Not found " + filename);
         } catch (IOException e) {
             System.out.println("IOException for " + filename);
         }
