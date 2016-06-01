1b9d2a1200230a6d083c9129aab4e476d2e0521c
Thomas Thüm <t.thuem@tu-braunschweig.de>
2016-06-01 18:53:23 +0200

    Changed default values of entries for citation service

--- MibTeX/src/de/mibtex/export/ExportCitations.java
+++ MibTeX/src/de/mibtex/export/ExportCitations.java
@@ -21,60 +21,63 @@ import de.mibtex.BibtexViewer;
 import de.mibtex.citationservice.CitationEntry;
 
 /**
- * A class that generates a .csv file with all BibTeX entries for the citation service
+ * A class that generates a .csv file with all BibTeX entries for the citation
+ * service
  * 
  * @author Christopher Sontag
  */
 public class ExportCitations extends Export {
 
-    public ExportCitations(String path, String file) throws Exception {
-        super(path, file);
-    }
+	public ExportCitations(String path, String file) throws Exception {
+		super(path, file);
+	}
 
-    @Override
-    public void writeDocument() {
-        List<CitationEntry> newCitations = new ArrayList<CitationEntry>();
-        List<CitationEntry> citations = readCitationFile(new File(BibtexViewer.CITATION_DIR,"citations.csv"));
-        for (BibtexEntry entry : entries.values()){
-            CitationEntry newEntry = new CitationEntry(entry.key,BibtexEntry.toURL(entry.title),0,(System.currentTimeMillis()-(25*60*60)));
-            if (citations.contains(newEntry)) {
-                newCitations.add(citations.get(citations.indexOf(newEntry)));
-            }
-            else {
-                newCitations.add(newEntry);
-            }
-        }
-        writeCitationFile(new File(BibtexViewer.CITATION_DIR,"citations.csv"),newCitations);    
-    }
-    
-    protected List<CitationEntry> readCitationFile(File file) {
-        System.out.print("Reading " + file.getName() + "... ");
-        List<CitationEntry> entries = new ArrayList<CitationEntry>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            for (String line; (line = br.readLine()) != null;) {
-                entries.add(CitationEntry.getFromCSV(line));
-            }
-        } catch (IOException e) {
-            System.out.println("IOException for " + file.getAbsolutePath());
-        }
-        System.out.println("done.");
-        return entries;
-    }
-    
-    protected void writeCitationFile(File file, List<CitationEntry> entries) {
-        System.out.print("Updating " + file.getName() + "... ");
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(file));
-            for (CitationEntry entry : entries) {
-                out.append(entry.getCSVString());
-            }
-            out.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Not found " + file.getAbsolutePath());
-        } catch (IOException e) {
-            System.out.println("IOException for " + file.getAbsolutePath());
-        }
-        System.out.println("done.");
-    }
-    
+	@Override
+	public void writeDocument() {
+		List<CitationEntry> newCitations = new ArrayList<CitationEntry>();
+		List<CitationEntry> citations = readCitationFile(new File(
+				BibtexViewer.CITATION_DIR, "citations.csv"));
+		for (BibtexEntry entry : entries.values()) {
+			CitationEntry newEntry = new CitationEntry(entry.key,
+					BibtexEntry.toURL(entry.title), -1, 0);
+			if (citations.contains(newEntry)) {
+				newCitations.add(citations.get(citations.indexOf(newEntry)));
+			} else {
+				newCitations.add(newEntry);
+			}
+		}
+		writeCitationFile(new File(BibtexViewer.CITATION_DIR, "citations.csv"),
+				newCitations);
+	}
+
+	protected List<CitationEntry> readCitationFile(File file) {
+		System.out.print("Reading " + file.getName() + "... ");
+		List<CitationEntry> entries = new ArrayList<CitationEntry>();
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			for (String line; (line = br.readLine()) != null;) {
+				entries.add(CitationEntry.getFromCSV(line));
+			}
+		} catch (IOException e) {
+			System.out.println("IOException for " + file.getAbsolutePath());
+		}
+		System.out.println("done.");
+		return entries;
+	}
+
+	protected void writeCitationFile(File file, List<CitationEntry> entries) {
+		System.out.print("Updating " + file.getName() + "... ");
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(file));
+			for (CitationEntry entry : entries) {
+				out.append(entry.getCSVString());
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Not found " + file.getAbsolutePath());
+		} catch (IOException e) {
+			System.out.println("IOException for " + file.getAbsolutePath());
+		}
+		System.out.println("done.");
+	}
+
 }
