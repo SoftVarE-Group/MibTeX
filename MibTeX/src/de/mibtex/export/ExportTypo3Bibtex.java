262ce594315d7e048ab3b538d95d48a813b9ef3d
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-11-24 15:45:24 +0100

    more fixes and refactorings and documentation

--- MibTeX/src/de/mibtex/export/ExportTypo3Bibtex.java
+++ MibTeX/src/de/mibtex/export/ExportTypo3Bibtex.java
@@ -1,16 +1,17 @@
 package de.mibtex.export;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CodingErrorAction;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -28,32 +29,41 @@ import de.mibtex.BibtexViewer;
 public class ExportTypo3Bibtex extends Export {
 	// TODO: Having numbers in Bibtex tags is not conforming the Bibtex standard.
 	private final static String Typo3TagsAttribute = "typo3Tags";
-
-	private static class Typo3Entry {
+	private final static String Thomas = "Thomas Thüm";
+	private final static String Chico = "Chico Sundermann";
+	private final static String Tobias = "Tobias Heß";
+	private final static String Paul = "Paul Maximilan Bittner";
+	
+	private static class Typo3Entry implements Comparable<Typo3Entry> {
+		final BibtexEntry source;
+		
 		String type; // inproceedings, article, ...
 		String key;
+		
 		String typeAttrib; // type = {...}
 		String title;
 		List<String> authors;
 		List<String> editors;
-		int year;
 		String booktitle;
-		
 		String address;
 		String publisher;
 		String journal;
 		String location;
-		
 		String school;
 		String pages;
+		int year;
 		
 		String doi;
 		String isbn;
 		String issn;
 		
+		String note;
+		
 		List<String> tags;
 		
 		public Typo3Entry(BibtexEntry bib, Map<String, String> variables) {
+			this.source = bib;
+			
 			this.type = bib.type;
 			this.typeAttrib = MakeTypo3Safe(GetAttribute(bib, BibTeXEntry.KEY_TYPE));
 			this.key = bib.key;
@@ -82,6 +92,8 @@ public class ExportTypo3Bibtex extends Export {
 			this.isbn = GetAttribute(bib, "isbn");
 			this.issn = GetAttribute(bib, "issn");
 			
+			this.note = MakeTypo3Safe(GetAttribute(bib, BibTeXEntry.KEY_NOTE));
+			
 			{
 				String booktitle;
 				if (Filters.Is_techreport.test(this)) {
@@ -105,7 +117,8 @@ public class ExportTypo3Bibtex extends Export {
 				this.tags = splitAttributeListString(this.tags);
 			}
 		}
-		
+
+		@Override
 		public String toString() {
 			String typo3 = "@" + type + "{" + key;
 
@@ -143,12 +156,24 @@ public class ExportTypo3Bibtex extends Export {
 			typo3 += GenBibTeXAttributeIfPresent("isbn", isbn);
 			typo3 += GenBibTeXAttributeIfPresent("issn", issn);
 			
+			typo3 += GenBibTeXAttributeIfPresent("note", note);
+			
 			typo3 += GenBibTeXAttributeIfPresent("tags", tags.stream().reduce("", (a, b) -> a + ", " + b));
 
 			return typo3 + "\n}";
 		}
+		
+		@Override
+		public boolean equals(Object other) {
+			return other != null && other instanceof Typo3Entry && ((Typo3Entry) other).title.equals(this.title);
+		}
+
+		@Override
+		public int compareTo(Typo3Entry other) {
+			return title.compareTo(other.title);
+		}
 	}
-	
+
 	private static class Filters {
 		final static Predicate<Typo3Entry> Is_misc = b -> b.type.equals("misc");
 		final static Predicate<Typo3Entry> Is_proceedings = b -> b.type.equals("proceedings");
@@ -158,11 +183,11 @@ public class ExportTypo3Bibtex extends Export {
 		final static Predicate<Typo3Entry> Is_mastersthesis = b -> b.type.equals("mastersthesis");
 		final static Predicate<Typo3Entry> Is_phdthesis = b -> b.type.equals("phdthesis");
 		
-		final static Predicate<Typo3Entry> WithThomas = Filters.AuthorOrEditorIsOneOf("Thomas Thüm");
-		final static Predicate<Typo3Entry> WithThomasAtISF = WithThomas.and(b -> b.year < 2020);
+		final static Predicate<Typo3Entry> WithThomas = Filters.AuthorOrEditorIsOneOf(Thomas);
+		final static Predicate<Typo3Entry> WithThomasBeforeUlm = WithThomas.and(b -> b.year < 2020);
 		final static Predicate<Typo3Entry> WithThomasAtUlm = WithThomas.and(b -> b.year >= 2020);
 		
-		final static Predicate<Typo3Entry> SoftVarE = Filters.AuthorOrEditorIsOneOf("Thomas Thüm", "Chico Sundermann", "Tobias Heß", "Paul Maximilian Bittner").and(b -> b.year >= 2020);
+		final static Predicate<Typo3Entry> SoftVarE = Filters.AuthorOrEditorIsOneOf(Thomas, Chico, Tobias, Paul).and(b -> b.year >= 2020);
 		
 		final static Predicate<Typo3Entry> BelongsToVariantSync = b -> {
 			if (b.tags == null) return false;
@@ -174,20 +199,28 @@ public class ExportTypo3Bibtex extends Export {
 		private static Predicate<Typo3Entry> KeyIsOneOf(String... keys) {
 			return b -> Arrays.asList(keys)
 					.stream()
-					.anyMatch((b.key)::equals);
+					.anyMatch(b.key::equals);
 		}
 		
 		private static Predicate<Typo3Entry> AuthorOrEditorIsOneOf(String... authors) {
-			return b -> Arrays.asList(authors)
-					.stream()
-					.anyMatch(author -> b.authors.contains(author) || b.editors.contains(author));
+			return AuthorIsOneOf(authors).or(EditorIsOneOf(authors));
+		}
+		
+		private static Predicate<Typo3Entry> AuthorIsOneOf(String... authors) {
+			return b -> AnyMatch(b.authors::contains, authors);
+		}
+		
+		private static Predicate<Typo3Entry> EditorIsOneOf(String... editors) {
+			return b -> AnyMatch(b.editors::contains, editors);
 		}
 		
 		@SafeVarargs
 		private static <T> Predicate<T> IsOneOf(T... elements) {
-			return s -> Arrays.asList(elements)
-					.stream()
-					.anyMatch(s::equals);
+			return s -> AnyMatch(s::equals, elements);
+		}
+		
+		private static <T> boolean AnyMatch(Predicate<T> condition, T... elements) {
+			return Arrays.asList(elements).stream().anyMatch(condition);
 		}
 		
 		private static <T> Predicate<T> Any() {
@@ -197,9 +230,15 @@ public class ExportTypo3Bibtex extends Export {
 	
 	private static class Modifiers {
 		public static Typo3Entry MarkThomasAsEditor(Typo3Entry t) {
-			// This is so pure .... not
 			if (t.editors.contains("Thomas Thüm")) {
 				t.tags.add("EditorialThomasThuem");
+			}
+			return t;
+		}
+		
+		public static Typo3Entry MarkTitleIfVenueIsSE(Typo3Entry t) {
+			if ("SE".equals(t.source.venue)) {
+				t.title += " (SE)";
 			}
 			return t;
 		}
@@ -230,11 +269,25 @@ public class ExportTypo3Bibtex extends Export {
 
 	@Override
 	public void writeDocument() {
-		// TODO: GI not found in MYabrv
-		// TODO: TO appear
-		// TODO: Doppelte Namen -> Hardcoded solution
-		// TODO: Warum fehlen noch 20? Publikationen von Thomas.
 		// TODO: Wiki-Eintrag
+		// TODO: TO appear -> Add to Booktitle?
+		// TODO: Warum fehlen noch 20? Publikationen von Thomas.
+		/**
+		 * 1.) Duplikate kann es online nicht geben, da Typo3 den Titel als Primary Key nimmt.
+		 * 2.) Manche Duplikate sind online aber dennoch vorhanden, weil das eine im Ordner "Publikationen" und das andere in "Alte Publikationen Thomas Thüm" liegt.
+		 * 
+		 * Stats:
+		 * A = Thomas ist Autor.
+		 * E = Thomas ist Editor.
+		 * ----------------------------------
+		 * Filter               |total|unique
+		 * ----------------------------------
+		 * A                    | 111 | 102
+		 * E                    |   5 |   5
+		 * A und E              |   0 |   0
+		 * A oder E             | 116 | 107
+		 * (A oder E) und !misc | 115 | 107
+		 */
 		
 		/**
 		 * Select the filter you need to export only the publications you are interested in.
@@ -246,9 +299,12 @@ public class ExportTypo3Bibtex extends Export {
 		 */
 		final Predicate<Typo3Entry> bibFilter =
 				//Filters.Any()
-				Filters.KeyIsOneOf("RSC+:SE21", "KJN+:SE21")
-				//Filters.SoftVarE
-				//Filters.WithThomas
+				//Filters.KeyIsOneOf("RSC+:SE21", "KJN+:SE21", "KJN+:FASE20")
+				//Filters.KeyIsOneOf("RSC+:SE21")
+				//Filters.AuthorIsOneOf("Thomas Thüm")
+				//Filters.EditorIsOneOf("Thomas Thüm")
+				Filters.AuthorIsOneOf(Thomas).or(Filters.EditorIsOneOf(Thomas))
+				.and(Filters.Is_misc.negate())
 		;
 		
 		/**
@@ -259,6 +315,7 @@ public class ExportTypo3Bibtex extends Export {
 		 */
 		final List<Function<Typo3Entry, Typo3Entry>> modifiers = Arrays.asList(
 				Modifiers::MarkThomasAsEditor
+				//, Modifiers::MarkTitleIfVenueIsSE
 		);
 
 		/**
@@ -266,19 +323,32 @@ public class ExportTypo3Bibtex extends Export {
 		 * It should not be necessary that you touch this.
 		 */
 		Map<String, String> variables = readVariablesFromBibtexFile(new File(BibtexViewer.BIBTEX_DIR, MYabrv));
-		String typo3 = entries.values().stream()
+		
+		List<Typo3Entry> typo3Entries = entries.values().stream()
 				.map(b -> new Typo3Entry(b, variables))
 				.filter(bibFilter)
 				.map(modifiers.stream().reduce(Function.identity(), Function::compose))
+				.collect(Collectors.toList());
+		
+		String typo3 = typo3Entries.stream()
 				.map(Typo3Entry::toString)
-/*
-				.count() + ""
-/*/
-				.reduce("", (a, b) -> a + "\n\n" + b)
-//*/
-				;
-
+				.reduce("", (a, b) -> a + "\n\n" + b);
+		
+		Collections.sort(typo3Entries);
+		
+		int duplicates = 0;
+		for (int i = 0; i < typo3Entries.size() - 1; ++i) {
+			if (typo3Entries.get(i).equals(typo3Entries.get(i + 1))) {
+				duplicates += resolveDuplicate(typo3Entries.get(i), typo3Entries.get(i + 1)) ? 0 : 1;
+			}
+		}
+		
+		long numUniqueEntries = typo3Entries.size() - duplicates;
+		
 		System.out.println(typo3);
+		System.out.println("\nExported " + typo3Entries.size() + " entries.");
+		System.out.println("Thereof " + numUniqueEntries + " entries are unique (by title).");
+		System.out.println();
 
         CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
         encoder.onMalformedInput(CodingErrorAction.REPORT);
@@ -286,28 +356,39 @@ public class ExportTypo3Bibtex extends Export {
 		writeToFile(new File(BibtexViewer.OUTPUT_DIR, "typo3.bib"), typo3, encoder);
 	}
 	
+	private boolean resolveDuplicate(Typo3Entry a, Typo3Entry b) {
+		System.out.println("Found duplicate " + a.title);
+		System.out.println("  ... remains unresolved.");
+		return false;
+	}
+
 	private Map<String, String> readVariablesFromBibtexFile(File pathToBibtex) {
 		Map<String, String> vars = new HashMap<String, String>();
 		
-		String[] file = readFromFile(pathToBibtex).split(System.lineSeparator());
-		for (String line : file) {
-			line = line.trim();
-			if (line.startsWith("@String")) {
-				line = line.substring(line.lastIndexOf("{") + 1);
-				String[] words = line.substring(0, line.indexOf("}")).split("=");
-				if (words.length == 2) {
-					String var = words[0].trim().toUpperCase();
-					// remove apostrophes ("") at the beginning and end
-					String val = words[1].trim().substring(1, words[1].length() - 2);
-					vars.put(var, val);
-//					System.out.println("[ExportTypo3Bibtex.readVariablesFromBibtexFile] " + var + " -> " + val);
+		BufferedReader file = readFromFile(pathToBibtex, Charset.forName("UTF-8"));
+		try {
+			while (file.ready()) {
+				String line = file.readLine().trim();
+				if (line.startsWith("@String")) {
+					line = line.substring(line.indexOf("{") + 1);
+					String[] words = line.substring(0, line.lastIndexOf("}")).split("=");
+					if (words.length == 2) {
+						String var = words[0].trim().toUpperCase();
+						// Remove apostrophes ("") at the beginning and end.
+						String val = words[1].trim().substring(1, words[1].length() - 2);
+						vars.put(var, val);
+						//System.out.println("[ExportTypo3Bibtex.readVariablesFromBibtexFile] " + var + " -> " + val);
+					}
 				}
 			}
+			file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
 		return vars;
 	}
 
-	
 	private static String lookup(String variable, final Map<String, String> variables) {
 		String value = variables.get(variable.toUpperCase());
 		return value == null ? variable : value;
@@ -336,20 +417,27 @@ public class ExportTypo3Bibtex extends Export {
 	
 	// Utils
 	
+	/**
+	 * Splits all strings in the input list to separate strings.
+	 * For example, given l = ["a", "b,c", "d;e,f"] as input, we get
+	 * splitAttributeListString(l) = ["a", "b", "c", "d" , "e", "f"].
+	 * @param tags
+	 * @return
+	 */
 	private static List<String> splitAttributeListString(List<String> tags) {
-		return Repeat(tags,
-				Arrays.asList(",", ";"),
-				(div, l) -> l.stream().collect(
-						() -> new ArrayList<String>(),
-						(list, kw) -> ((List<String>)list).addAll(Arrays.stream(kw.split(div)).map(s -> s.trim()).collect(Collectors.toList())
-								),
-						(list1, list2) -> list1.addAll(list2)));
-	}
-	
-	private static <T, A> T Repeat(T t, Collection<A> args, BiFunction<A, T, T> f) {
-		for (A a : args) {
-			t = f.apply(a,  t);
-		}
-		return t;
+		Function<String, Function<List<String>, List<String>>> splitByDivider = div -> l -> {
+			return l.stream().collect(
+					() -> new ArrayList<String>(),
+					(list, kw) -> ((List<String>)list).addAll(
+							Arrays.stream(kw.split(div))
+							.map(String::trim)
+							.collect(Collectors.toList())),
+					(list1, list2) -> list1.addAll(list2));
+		};
+		
+		return Arrays.asList(",", ";").stream()
+				.map(splitByDivider)
+				.reduce(Function.identity(), Function::compose)
+				.apply(tags);
 	}
 }
