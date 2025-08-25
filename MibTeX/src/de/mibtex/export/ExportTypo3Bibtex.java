5da643295d67a3099e793c8af2c101f3d8f0f174
pmbittner <paul.bittner@uni-ulm.de>
2025-08-25 08:45:57 +0200

    rules for new duplicates in BibTags

--- MibTeX/src/de/mibtex/export/ExportTypo3Bibtex.java
+++ MibTeX/src/de/mibtex/export/ExportTypo3Bibtex.java
@@ -166,6 +166,8 @@ public class ExportTypo3Bibtex extends Export {
             , whenKeyIs("BSM+:OOPSLA24", sideffect(t -> t.journal += " " + t.source.entry.getField(BibTeXEntry.KEY_NUMBER).toUserString()))
 
             // Resolving duplicates
+            , whenKeyIs("KMP+:TR25subsumedbyKMP+:TOSEM25", MARK_AS_TECHREPORT)
+            , whenKeyIs("KTPS:TR18subsumedbyKTPS:FIDE18", MARK_AS_TECHREPORT)
             , whenKeyIs("PKTS:TR24subsumedbyPKTS:SPLC24", MARK_AS_TECHREPORT)
             , whenKeyIs("SKH+:SPLC24", MARK_AS_JOURNAL_FIRST)
             , whenKeyIs("RBP+:TR22subsumedbyRBP+:LMCS23", MARK_AS_TECHREPORT)
